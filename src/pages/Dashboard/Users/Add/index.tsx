c61f13aa347f4291c572abf1e5245883adcec4af
Alja Čekada <alja.cekada@gmail.com>
2023-10-11 14:25:41 -0700

    create, update, delete users

--- src/pages/Dashboard/Users/Add/index.tsx
+++ src/pages/Dashboard/Users/Add/index.tsx
@@ -1,8 +1,14 @@
 import DashboardLayout from 'components/ui/DashboardLayout'
-import React, { FC } from 'react'
+import CreateUpdateUserForm from 'components/user/CreateUpdateUserForm/CreateUpdateUserForm'
+import { FC } from 'react'
 
 const DashboardUsersAdd: FC = () => {
-  return <DashboardLayout>DashboardUsersAdd</DashboardLayout>
+  return (
+    <DashboardLayout>
+      <h1 className="mb-4 text-center">Create new user</h1>
+      <CreateUpdateUserForm />
+    </DashboardLayout>
+  )
 }
 
 export default DashboardUsersAdd
--- src/pages/Dashboard/Users/Edit/index.tsx
+++ src/pages/Dashboard/Users/Edit/index.tsx
@@ -1,8 +1,16 @@
 import DashboardLayout from 'components/ui/DashboardLayout'
-import React, { FC } from 'react'
+import CreateUpdateUserForm from 'components/user/CreateUpdateUserForm/CreateUpdateUserForm'
+import { FC } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const DashboardUsersEdit: FC = () => {
-  return <DashboardLayout>DashboardUsersEdit</DashboardLayout>
+  const location = useLocation()
+  return (
+    <DashboardLayout>
+      <h1 className="mb-4 text-center">Edit user</h1>
+      <CreateUpdateUserForm defaultValues={location.state} />
+    </DashboardLayout>
+  )
 }
 
 export default DashboardUsersEdit
